d23de794ed42dcbc925913655b2d56675bd4d08a
Marko Petrović <petrovicmarko2006@gmail.com>
2025-07-29 17:28:09 +0200

    Optimization update. Don't spawn unnecessary processes for already obtained information.

--- src/sharedextents.cpp
+++ src/sharedextents.cpp
@@ -15,9 +15,15 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <libgen.h>
+#include <stdint.h>
 
 #include <unordered_set>
 #include <string>
+#include <utility>
+#include <functional>
+#include <vector>
+
+static bool debug_output = false;
 
 void statx_info(struct statx *info, int fd, const char *filename)
 {
@@ -69,7 +75,55 @@ struct process run_program(const char *argv[])
 	return ret;
 }
 
-std::unordered_set<std::string> handle_tree_dump(int fd, char *pattern, char *dirpath)
+using file_id = std::pair<uint64_t, uint64_t>;
+inline void hash_combine(size_t *seed, size_t hash)
+{
+	/* Magic constant from Boost (fractional part of the golden ratio) */
+	const size_t golden = 0x9e3779b9;
+
+	/* Combine the new hash into the seed */
+	*seed ^= hash + golden + (*seed << 6) + (*seed >> 2);
+}
+/* Specialize std::hash for file_id */
+namespace std
+{
+	template <>
+	struct hash<file_id> {
+		size_t operator()(const file_id &fid) const
+		{
+			size_t seed = 0;
+			hash_combine(&seed, hash<uint64_t>{}(fid.first));
+			hash_combine(&seed, hash<uint64_t>{}(fid.second));
+			return seed;
+		}
+	};
+}
+
+file_id extract_id(const char *buf)
+{
+	unsigned long long int subvolid, inode;
+	char subvolid_str[21];
+	int ret = sscanf(buf, "%*[^)]) extent data backref root %s objectid %llu", &subvolid_str, &inode);
+	if (ret != 2) {
+		if (debug_output)
+			fprintf(stderr, "Failed to read subvolume ID from string \"%s\"", buf);
+		return {0,0};
+	}
+	if (!strcmp(subvolid_str, "FS_ROOT")) {
+		subvolid = 5;
+	}
+	else {
+		ret = sscanf(subvolid_str, "%llu", &subvolid);
+		if (ret != 1) {
+			if (debug_output)
+				fprintf(stderr, "Failed to read subvolume ID from string \"%s\"", buf);
+			return {0,0};
+		}
+	}
+	return {inode, subvolid};
+}
+
+std::unordered_set<std::string> handle_tree_dump(int fd, file_id target, char *dirpath)
 {
 	const int buf_size = 16*1024;
 	char *buf = (char*) malloc(buf_size);
@@ -85,30 +139,19 @@ std::unordered_set<std::string> handle_tree_dump(int fd, char *pattern, char *di
 	#define PRESTART 0
 	#define START 1
 	#define REFLINE 2
-	#define BACKREF 3
+	#define BACKREF_SPECULATIVE 3
+	#define BACKREF 4
 	int state = PRESTART;
 	char logical_addr[128];	/* Captured as string to pass it to btrfs command */
 	int refcount;
 	std::unordered_set<std::string> result;
+	std::vector<file_id> speculative_visited_ids;
+	std::unordered_set<file_id> visited_ids;
+	size_t old_size = 0;
 	while (fgets(buf, len, fp)) {
-		if (strstr(buf, "item"))
-			state = START;
-		if (state == START) {
-			sscanf(buf, "%*[ \t]item %*llu key (%s", logical_addr);
-			state = REFLINE;
-			continue;
-		}
-		if (state == REFLINE) {
-			sscanf(buf, "%*[ \t]refs %d", &refcount);
-			if (refcount > 1)
-				state = BACKREF;
-			else
-				state = PRESTART;
-			continue;
-		}
-		if (state == BACKREF) {
-			/* Found our extent */
-			if (strstr(buf, pattern)) {
+		if (strstr(buf, "item")) {
+			if (state == BACKREF && visited_ids.size() > old_size) {
+				/* There was new ID spotted */
 				const char *argv[] = {"btrfs", "inspect-internal", "logical-resolve", logical_addr, dirpath, NULL};
 				struct process btrfsproc = run_program(argv);
 				FILE *filepath_stream = fdopen(btrfsproc.pipefd, "r");
@@ -134,13 +177,50 @@ std::unordered_set<std::string> handle_tree_dump(int fd, char *pattern, char *di
 					exit(1);
 				}
 				fclose(filepath_stream);
-				state = PRESTART;
 			}
+			old_size = visited_ids.size();
+			speculative_visited_ids.clear();
+			state = START;
+		}
+		if (state == START) {
+			int ret = sscanf(buf, "%*[ \t]item %*llu key (%s", logical_addr);
+			if (ret == 1)
+				state = REFLINE;
+			else
+				state = PRESTART;
+			continue;
+		}
+		if (state == REFLINE) {
+			int ret = sscanf(buf, "%*[ \t]refs %d", &refcount);
+			if (refcount > 1 && ret == 1)
+				state = BACKREF_SPECULATIVE;
+			else
+				state = PRESTART;
+			continue;
+		}
+		if (state == BACKREF_SPECULATIVE) {
+			file_id this_extent = extract_id(buf);
+			if (this_extent == file_id{0,0})
+				continue;
+			if (this_extent == target) {
+				for (const file_id &id : speculative_visited_ids) {
+					visited_ids.insert(id);
+				}
+				state = BACKREF;
+			}
+			else {
+				speculative_visited_ids.push_back(this_extent);
+			}
+		}
+		if (state == BACKREF) {
+			file_id this_extent = extract_id(buf);
+			if (this_extent == file_id{0,0})
+				continue;
+			visited_ids.insert(this_extent);
 		}
 	}
 	fclose(fp);
 	free(buf);
-	free(pattern);
 	
 	return result;
 }
@@ -167,7 +247,17 @@ void find_device(char *dev, char *dirpath)
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2) {
+	if (argc == 3) {
+		if (!strcmp(argv[1], "-d")) {
+			debug_output = true;
+			argv[1] = argv[2];
+		}
+		else {
+			fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+			return 1;
+		}
+	}
+	if (argc < 2) {
 		if (argc == 1)
 			fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
 		else
@@ -184,15 +274,6 @@ int main(int argc, char *argv[])
 	struct statx info;
 	statx_info(&info, fd, argv[1]);
 	close(fd);
-	
-	char *pattern;
-	int ret;
-	if (info.stx_subvol == 5)
-		ret = asprintf(&pattern, "extent data backref root FS_ROOT objectid %llu", info.stx_ino);
-	else
-		ret = asprintf(&pattern, "extent data backref root %llu objectid %llu", info.stx_subvol, info.stx_ino);
-	if (ret < 0)
-		return ENOMEM;
 
 	char dev[PATH_MAX];
 	char *dirpath = dirname(argv[1]);
@@ -200,8 +281,7 @@ int main(int argc, char *argv[])
 
 	const char *newargv[] = {"btrfs", "inspect-internal", "dump-tree", "-t", "extent", dev, NULL};
 	struct process btrfsproc = run_program(newargv);
-	/* Takes ownership of pattern */
-	auto result = handle_tree_dump(btrfsproc.pipefd, pattern, dirpath);
+	auto result = handle_tree_dump(btrfsproc.pipefd, {info.stx_ino, info.stx_subvol}, dirpath);
 	siginfo_t cmd_state;
 	if (waitid(P_PID, btrfsproc.pid, &cmd_state, WEXITED)) {
 		perror("waitid");
